691b1b2b642fbdc650e7a3d625312e25b960d910
Dmitrii Nikoshin <d.nikoshin@gmail.com>
2025-06-23 21:15:29 +0300

    task 1-3 added presenter and view modules

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import BoardPresenter from './presenter/presenter.js';
+import Presenter from './presenter/presenter.js';
 
+const tripMainElement = document.querySelector('.trip-main');
 const filterContainer = document.querySelector('.trip-controls__filters');
 const eventsContainer = document.querySelector('.trip-events');
 
-const boardPresenter = new BoardPresenter({
+const boardPresenter = new Presenter({
+  tripMainElement,
   filterContainer,
   eventsContainer,
 });
--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -6,22 +6,22 @@ import EditFormView from '../view/edit-form-view.js';
 import TripTitleView from '../view/trip-title-view.js';
 import { render, RenderPosition } from '../render.js';
 
-export default class BoardPresenter {
-  constructor({ filterContainer, eventsContainer, listContainer }) {
+export default class Presenter {
+  constructor({ tripMainElement, filterContainer, eventsContainer }) {
+    this.tripMainElement = tripMainElement;
     this.filterContainer = filterContainer;
     this.eventsContainer = eventsContainer;
     this.eventListComponent = new EventListView();
-    this.listContainer = listContainer;
   }
 
   init() {
+    render(new TripTitleView(), this.tripMainElement, RenderPosition.AFTERBEGIN);
+
     render(new FilterView(), this.filterContainer);
 
     render(new SortView(), this.eventsContainer);
 
     render(this.eventListComponent, this.eventsContainer);
-
-    render(new TripTitleView(), this.listContainer.getElement(), RenderPosition.BEFOREBEGIN);
 
     render(new EditFormView(), this.eventListComponent.getElement(), RenderPosition.AFTERBEGIN);
 
